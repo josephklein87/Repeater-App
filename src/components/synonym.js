9b071df082d062c625c97cd0528aa01bc6ea4038
Joe Klein SEI <josephklein87@gmail.com>
2023-03-15 23:37:32 -0400

    wrote loop to remove underscores from data results

--- src/components/synonym.js
+++ src/components/synonym.js
@@ -4,26 +4,29 @@ import '../App.css';
 
 function Synonym(props) {
     let [synonymsList, setSynonymsList] = useState([]);
-    let found = false
     
     const synonymFinder = () => {
         // axios.get(`https://api.datamuse.com/words?rel_syn=${props.word}`).then((res)=>{
         // console.log(res.data)
         //     setSynonyms(res.data)
         // })
-        if (found === false) {
         axios.get(`https://api.api-ninjas.com/v1/thesaurus?word=${props.word}`, {
             headers: {'X-Api-Key': process.env.REACT_APP_API_KEY}},
             ).then((res)=>{
                 console.log(res.data.synonyms)
-                    setSynonymsList(res.data.synonyms)
-                    found = true
-    })
-        }}
+                    let synonymArray = res.data.synonyms
+                    for (let i = 0; i < synonymArray.length; i++) {
+                        let replaced = synonymArray[i].replace("_", " ")
+                        synonymArray[i] = replaced
+                    }
+                    setSynonymsList(synonymArray)
+                    
+    })}
 
     useEffect(() => {
         synonymFinder();
     }, [])
+
     
 
     return (
