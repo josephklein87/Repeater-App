595e0d82e3b20d54baa1aca9007121cbced15b50
Joe Klein SEI <josephklein87@gmail.com>
2023-03-16 13:39:04 -0400

    changed back to data muse api

--- src/components/synonym.js
+++ src/components/synonym.js
@@ -6,22 +6,22 @@ function Synonym(props) {
     let [synonymsList, setSynonymsList] = useState([]);
     
     const synonymFinder = () => {
-        // axios.get(`https://api.datamuse.com/words?rel_syn=${props.word}`).then((res)=>{
-        // console.log(res.data)
-        //     setSynonyms(res.data)
-        // })
-        axios.get(`https://api.api-ninjas.com/v1/thesaurus?word=${props.word}`, {
-            headers: {'X-Api-Key': process.env.REACT_APP_API_KEY}},
-            ).then((res)=>{
-                console.log(res.data.synonyms)
-                    let synonymArray = res.data.synonyms
-                    for (let i = 0; i < synonymArray.length; i++) {
-                        let replaced = synonymArray[i].replace("_", " ")
-                        synonymArray[i] = replaced
-                    }
-                    setSynonymsList(synonymArray)
+        axios.get(`https://api.datamuse.com/words?rel_syn=${props.word}`).then((res)=>{
+        console.log(res.data)
+            setSynonymsList(res.data)
+        })
+        // axios.get(`https://api.api-ninjas.com/v1/thesaurus?word=${props.word}`, {
+        //     headers: {'X-Api-Key': placeholder},
+        //     ).then((res)=>{
+        //         console.log(res.data.synonyms)
+        //             let synonymArray = res.data.synonyms
+        //             for (let i = 0; i < synonymArray.length; i++) {
+        //                 let replaced = synonymArray[i].replaceAll("_", " ")
+        //                 synonymArray[i] = replaced
+        //             }
+        //             setSynonymsList(synonymArray)
                     
-    })}
+    }
 
     useEffect(() => {
         synonymFinder();
@@ -32,7 +32,7 @@ function Synonym(props) {
     return (
         <>
             {synonymsList.map((synonym)=>{
-                return <p className='synonym-single'>{synonym}</p>
+                return <p className='synonym-single'>{synonym.word}</p>
             })}
         </>
     )
